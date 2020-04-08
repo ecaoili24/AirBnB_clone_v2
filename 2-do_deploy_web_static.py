ce22f1839c90229ad182a4f47dba69723e5d8d00
ecaoili24 <erika.caoili>
2020-04-08 03:42:54 +0000

    Update to the exceptions

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,10 +4,10 @@ Fabric script based on the file 1-pack_web_static.py that distributes an
 archive to the web servers
 """
 import os.path
-from fabric.api import put, run, env
+from fabric.api import *
 from fabric.contrib import files
 
-
+env.user = "ubuntu"
 env.hosts = ['34.74.200.157', '107.20.131.122']
 
 
@@ -15,18 +15,19 @@ def do_deploy(archive_path):
     """distributes an archive to the web servers"""
     if not os.path.isfile(archive_path):
         return False
-    a_file = path = os.path.a_file(archive_path)
-    no_extenstion = os.path.splitext(a_file)
-    path = "/data/web_static/releases/".format(no_extension)
+    basename = path = os.path.basename(archive_path)
+    root = os.path.splitext(basename)
+    target = "/data/web_static/releases/".format(root)
     try:
         put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_extension))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(a_file, path, no_extension))
-        run('rm /tmp/{}'.format(a_file))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_extension))
-        run('rm -rf {}{}/web_static'.format(path, no_extension))
+        run('mkdir -p {}{}/'.format(target))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(path, target))
+        run('rm /tmp/{}'.format(path))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(target, target))
+        run('rm -rf {}{}/web_static'.format(target))
         run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_extension))
-        return True
+        run('ln -s {}{}/ /data/web_static/current'.format(target))
     except:
+        return False
+    else:
         return False
