15625799385c5f83e2aca6b44c53a2d85fad3763
ecaoili24 <erika.caoili@gmail.com>
2020-04-21 23:37:40 +0000

    Fixed a typo

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 @app.route('/states_list', strict_slashes=False)
 def show_states():
     """Displays states"""
-    state_dict = storage.all(States)
+    state_dict = storage.all(State)
     return render_template('7-states_list.html', state_dict=state_dict)
 
 
