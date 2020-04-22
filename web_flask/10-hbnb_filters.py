cf17b44ae375f8ebd85688b28be4f92c7a63dd8e
ecaoili24 <erika.caoili@gmail.com>
2020-04-22 23:58:53 +0000

    Initial commit of task 11

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+A script that starts a Flask web application
+"""
+
+from flask import Flask
+from flask import render_template
+from flask import escape
+from models import storage
+from models import State
+from models import Amenity
+
+app = Flask(__name__)
+
+
+@app.route('/hbnb_filters', strict_slashes=False)
+def show_filters():
+    """Displays states, cities, and amenities under filters"""
+    state_dict = storage.all(State)
+    amenity_dict = storage.all(Amenity)
+    return render_template('10-hbnb_filters.html', state_dict=state_dict,
+                           amenity_dict=amenity_dict)
+
+
+@app.teardown_appcontext
+def teardown_db(self):
+    """Removes current SQLAlchemy Session"""
+    storage.close()
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
